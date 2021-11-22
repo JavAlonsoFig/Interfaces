1b7d55ca220a171772f11d6b35cefa8a64613416
JavAlonsoFig <javier.alonsofig@gmail.com>
2021-11-22 19:00:37 +0100

    test

---
+++ main.py
@@ -0,0 +1,17 @@
+from menuSuperBoton import *
+import sys
+
+
+class Main (QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+
+    app = QtWidgets.QApplication([])
+    window = Main()
+    window.show()
+    sys.exit(app.exec())
